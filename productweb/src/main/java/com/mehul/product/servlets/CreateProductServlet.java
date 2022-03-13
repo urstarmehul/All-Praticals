48f2ce231061cd4730c12942acc212a1d0c4925f
Mehul Kumar <100113904+urstarmehul@users.noreply.github.com>
2022-03-13 14:38:15 +0530

    product_parent

---
+++ productweb/src/main/java/com/mehul/product/servlets/CreateProductServlet.java
@@ -0,0 +1,21 @@
+package com.mehul.product.servlets;
+
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateProductServlet
+ */
+
+public class CreateProductServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ productweb/src/main/java/com/mehul/product/servlets/DisplayProductDetailsServlet.java
@@ -0,0 +1,21 @@
+package com.mehul.product.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class DisplayProductDetailsServlet
+ */
+public class DisplayProductDetailsServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
