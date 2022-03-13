ac52f18df70a2cac96ffa4ffbb48842300195b0e
Mehul Kumar <100113904+urstarmehul@users.noreply.github.com>
2022-03-13 14:29:24 +0530

    junit

---
+++ junit-in-5-steps/test/com/in28minutes/junit/AssertTest.java
@@ -0,0 +1,19 @@
+package com.in28minutes.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AssertTest {
+
+	@Test
+	public void test() {
+		boolean condn = true;
+		assertEquals(true, condn);
+		assertTrue(condn);
+		assertFalse(condn);
+		
+		
+	}
+
+}
